41c7990bf8c514a0266ac1de3a7d9b9c23d068f0
Gurjas Singh <gsingh.ont@gmail.com>
2023-04-11 22:49:09 -0400

    semi functioning polygon tool

--- tools.py
+++ tools.py
@@ -4,9 +4,21 @@ import queue
 WIDTH = 800
 HEIGHT = 600
 
+
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 
 running =True
+
+tool = "polygon"
+polygonFirst = True
+
+# colours
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
 
 class Fill:
     def __init__(self, colour, colourClicked):
@@ -23,7 +35,7 @@ class Fill:
         neighbors = []
         for dx in range(-1, 2):
             for dy in range(-1, 2):
-                if x-dx > 0 and x-dx < WIDTH and y - dy > 0 and y - dy < HEIGHT and self.checkedVisited(x-dx, y-dy): 
+                if ( x-dx > 0 and x-dx < WIDTH and y - dy > 0 and y - dy < HEIGHT ) and self.checkedVisited(x-dx, y-dy): 
                     neighbors.append((x-dx, y - dy))
                     self.visited.append((x-dx, y - dy))
             
@@ -46,20 +58,77 @@ class Fill:
                     q.put(neighboringNodes)
         pygame.display.flip()
 
+
+class Polygon:
+    def __init__(self):
+        self.points = []
+        self.startCoor = ()
+        self.prevCoor = ()
+    
+    def firstCircle(self, x, y):
+        self.startCoor = (x, y)
+        self.prevCoor = (x, y)
+        pygame.draw.circle(screen, GREEN, (x, y), 6)
+    
+    def nextCircle(self, x, y, size, colour):
+
+        pygame.draw.line(screen, BLACK, self.prevCoor, (x, y), size)
+        pygame.draw.circle(screen, BLACK, self.prevCoor, 6)
+        pygame.draw.circle(screen, RED, (x, y), 6)
+
+        if self.prevCoor == self.startCoor:
+            pygame.draw.circle(screen, GREEN, self.startCoor, 6)
+        
+        self.prevCoor = (x, y)
+    
+    def endCircle(self, colour):
+
+        pygame.draw.line(screen, BLACK, self.prevCoor, self.startCoor)
+        pygame.draw.circle(screen, (0, 0, 0), self.prevCoor, 6)
+        pygame.draw.circle(screen, (0, 0, 0), self.startCoor, 6)
+
+
+
+
+
 FillTool = Fill((0, 0, 0), (255, 255, 255))
 
 
 
+
+cnt = 0
+
 screen.fill((255, 255, 255))
 while running:
+    mx, my = pygame.mouse.get_pos()
+    mb = pygame.mouse.get_pressed()
+
     for evt in pygame.event.get():
         if evt.type == pygame.QUIT:
             running = False
-
+        
+        if evt.type == pygame.MOUSEBUTTONDOWN:
+            if tool == "polygon":
+                if polygonFirst:
+                    poly = Polygon()
+                    poly.firstCircle(mx, my)
+                    polygonFirst = False
+                elif poly.startCoor[0] - 6 <= mx and poly.startCoor[0] + 6 >= mx and poly.startCoor[1] - 6 <= my and poly.startCoor[1] + 6 >= my:
+                    poly.endCircle((0, 0, 0))
+                    polygonFirst = True
+                    continue
+                else:
+                    poly.nextCircle(mx, my, 6, (0, 0, 0))
+                    cnt += 1
+            
+            if tool == "fill":
+                FillTool.fill(mx, my)
+                
+    if cnt > 5:
+        tool = "fill"
     # ----------------------------------
     # Your code goes here.
-    mx, my = pygame.mouse.get_pos()
-    mb = pygame.mouse.get_pressed()
+
     pygame.draw.line(screen, (0, 0, 0), (25, 25), (50, 25))
     pygame.draw.line(screen, (0, 0, 0), (25, 25), (25, 50))
     pygame.draw.line(screen, (0, 0, 0), (25, 50), (50, 50))
@@ -67,9 +136,9 @@ while running:
 
     pygame.draw.line(screen, (0, 0, 0), (25, 100), (50, 100))
     pygame.draw.line(screen, (0, 0, 0), (25, 100), (25, 150))
-    pygame.draw.line(screen, (0, 0, 0), (25, 150), (50, 150))
-    pygame.draw.line(screen, (0, 0, 0), (50, 150), (50, 100))
-    if mb[0]:
+    pygame.draw.line(screen, (0, 0, 0), (25, 150), (150, 150))
+    pygame.draw.line(screen, (0, 0, 0), (150, 150), (50, 100), 4)
+    if mb[2]:
         FillTool.fill(mx, my)
     # ----------------------------------
     pygame.display.flip()
